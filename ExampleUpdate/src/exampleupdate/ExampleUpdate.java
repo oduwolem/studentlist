8f7f00c53c995e9628bfceca38fa654a883d86bf
oduwolem <59750210+oduwolem@users.noreply.github.com>
2020-01-24 16:02:01 -0500

    Update ExampleUpdate.java

--- ExampleUpdate/src/exampleupdate/ExampleUpdate.java
+++ ExampleUpdate/src/exampleupdate/ExampleUpdate.java
@@ -6,8 +6,9 @@ public class ExampleUpdate {
     // this is the branch
     public static void main(String[] args) {
         System.out.println("pull operation");
-                System.out.println("pull operation 55");
-                System.out.println("fetch operation");
+                System.out.println("fetch operation 55");
+                System.out.println("merge operation");
+                System.out.println("branch operation");
     }
     
 }
